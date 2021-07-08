4981f6a372ac29cc431298aa0a8b3fcb028681e3
ashishghodke97 <srj246ag@gmail.com>
2021-07-08 11:31:17 +0530

    2nd project

---
+++ math.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int n1,n2;
+	printf("enter the 2 numbers\n");
+	scanf("%d%d",&n1,&n2);
+	printf("the Addition is %d \n",n1+n2);
+	printf("the substraction is %d \n",n1-n2);
+	printf("the multiplication is %d \n",n1*n2);
+	printf("the Division is  %d\n",n1/n2);
+	return 0;
+}
